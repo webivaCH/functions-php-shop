5b1ad079319af425b4aba1b22cdf5d3801a343cf
jsuchet <jysuchet@gmail.com>
2022-05-02 15:08:39 +0200

    Update functions.php with latest version (calmon.ch)

--- functions.php
+++ functions.php
@@ -29,8 +29,17 @@ function year_shortcode() {
 }
 add_shortcode('year', 'year_shortcode');
 
-
-add_action( 'login_enqueue_scripts', 'my_login_logo_one' );
+//Change WP logo for login or reset password
+function my_login_logo_one() { 
+?> 
+<style type="text/css"> 
+body.login div#login h1 a {
+ background-image: url(https://www.webiva.ch/wp-content/uploads/WEBIVA-Favicon.svg);
+padding-bottom: 10px; 
+} 
+</style>
+ <?php 
+} add_action( 'login_enqueue_scripts', 'my_login_logo_one' );
 
 /**
  * Custom admin login header link
@@ -47,42 +56,33 @@ function remove_menus(){
 	$roles = wp_get_current_user()->roles;
 
 	// test role
-	if( !in_array('editor',$roles)){
+	if( !in_array('contributor',$roles)){
 	return;
 	}
 
+	$ptype = 'elementor_library';
+	
 	//remove menu from site backend.
+	remove_menu_page( 'edit-comments.php' ); // Comments 
+	remove_menu_page( 'tools.php' ); // Tools
+// 	remove_menu_page( 'edit-tags.php?taxonomy=elementor_library_category' ); // elementor category
+// 	remove_menu_page( 'edit.php?post_type=elementor_library&tabs_group=library' ); // elementor category
 	remove_menu_page( 'edit.php?post_type=elementor_library' ); // Elementor Templates
 	remove_menu_page( 'elementor' ); // Elementor
+// 	remove_submenu_page( 'edit.php?post_type={$ptype}', 'edit-tags.php?taxonomy=elementor_library_category&amp;post_type={$ptype}' );
+	remove_menu_page( '/#menu-posts-elementor_library');
+	
 }
-add_action( 'admin_menu', 'remove_menus' , 100 );
-
-// puzzle to 404 page
-//https://codepen.io/onge/pen/LEzGRG
-
-function wpb_adding_scripts() {
-	  wp_enqueue_style( 'parent-style', get_stylesheet_directory_uri() . '/puzzle.css');
-
-    wp_register_script('my_amazing_script', get_stylesheet_directory_uri() . '/puzzle.js', array('jquery'),'1.1', true);
-    wp_enqueue_script('my_amazing_script');
-} 
+add_action( 'admin_menu', 'remove_menus' , 999 );
 
 
-
-add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts', 999 ); 
-
-//Change WP logo for login or reset password
-function my_login_logo_one() { 
-?>
-	<style type="text/css"> 
-	body.login div#login h1 a {
-	 background-image: url(https://www.webiva.ch/wp-content/uploads/WEBIVA-Favicon.svg);
-	padding-bottom: 10px; 
-	}		       
-	</style>
-<?
-}		       
-
-
-
-
+// Maps posts capability to elementor_library post type capabilities  
+function editing_elementor_library_capability($args, $post_type)
+{
+  if ('elementor_library' === $post_type) {
+    $args['map_meta_cap'] = true;
+    $args['capability_type'] = 'elementor_library'; // You could set your capability type name here
+  }
+  return $args;
+}
+add_filter('register_post_type_args', 'editing_elementor_library_capability', 10, 2);
